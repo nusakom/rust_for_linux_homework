3166bcd328d4b42d13a51ca6b6cb76b08b583489
Xiaoding Liu <xiaoding@users.noreply.github.com>
2023-11-06 01:35:12 +0800

    Update kernel boot argument in  build_image.sh
    Add "no_timer_check" argument in kernel boot cmd line.
    Avoid QEMU kernel panic at boot time.
    See https://lore.kernel.org/all/20200402093132.GA15839@Red/T/ for more information.

--- src_e1000/build_image.sh
+++ src_e1000/build_image.sh
@@ -42,7 +42,7 @@ qemu-system-x86_64 \
 -device "e1000,netdev=eth0" \
 -object "filter-dump,id=eth0,netdev=eth0,file=dump.dat" \
 -kernel $kernel_image \
--append "root=/dev/ram rdinit=sbin/init ip=10.0.2.15::10.0.2.1:255.255.255.0 console=ttyS0" \
+-append "root=/dev/ram rdinit=sbin/init ip=10.0.2.15::10.0.2.1:255.255.255.0 console=ttyS0 no_timer_check" \
 -nographic \
 -initrd $rootfs_img
 
